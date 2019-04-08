a9b476d592545e6d74f1e053c105ebc8224c55fe
dashdeckers <dashdeckers@gmail.com>
2019-04-08 13:33:03 +0200

    added pip2 and curl as dependencies

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
+sudo apt install python-pip curl libncurses5-dev libgnome2-dev libgnomeui-dev \
 libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
 libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
 python3-dev git ctags
